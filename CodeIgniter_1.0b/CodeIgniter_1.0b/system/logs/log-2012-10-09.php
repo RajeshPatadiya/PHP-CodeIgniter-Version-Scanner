64186a7a24d3b0f59126ecb5eaf1e043d42b1ed3
Kevin Morssink <contact@kevinmorssink.nl>
2015-08-03 10:20:21 +0200

    First commit

---
+++ CodeIgniter_1.0b/CodeIgniter_1.0b/system/logs/log-2012-10-09.php
@@ -0,0 +1,8 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-10-09 23:11:54 --> Severity: 8192  --> Function set_magic_quotes_runtime() is deprecated C:\xampp\htdocs\codeigniter-version-detect\CodeIgniter_1.0b\CodeIgniter_1.0b\system\libraries\Front_controller.php 40
+ERROR - 2012-10-09 23:12:55 --> Severity: 8192  --> Function set_magic_quotes_runtime() is deprecated C:\xampp\htdocs\codeigniter-version-detect\CodeIgniter_1.0b\CodeIgniter_1.0b\system\libraries\Front_controller.php 40
+ERROR - 2012-10-09 23:23:09 --> Severity: 8192  --> Function set_magic_quotes_runtime() is deprecated C:\xampp\htdocs\codeigniter-version-detect\CodeIgniter_1.0b\CodeIgniter_1.0b\system\libraries\Front_controller.php 40
+ERROR - 2012-10-09 23:40:29 --> Severity: 8192  --> Function set_magic_quotes_runtime() is deprecated C:\xampp\htdocs\codeigniter-version-detect\CodeIgniter_1.0b\CodeIgniter_1.0b\system\libraries\Front_controller.php 40
+ERROR - 2012-10-09 23:46:09 --> Severity: 8192  --> Function set_magic_quotes_runtime() is deprecated C:\xampp\htdocs\codeigniter-version-detect\CodeIgniter_1.0b\CodeIgniter_1.0b\system\libraries\Front_controller.php 40
+ERROR - 2012-10-09 23:48:38 --> Severity: 8192  --> Function set_magic_quotes_runtime() is deprecated C:\xampp\htdocs\codeigniter-version-detect\CodeIgniter_1.0b\CodeIgniter_1.0b\system\libraries\Front_controller.php 40
---
+++ CodeIgniter_1.0b/CodeIgniter_1.0b/system/logs/log-2015-03-25.php
@@ -0,0 +1,3 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2015-03-25 20:42:59 --> Severity: 8192  --> Function set_magic_quotes_runtime() is deprecated C:\Users\KM\Desktop\CodeIgniterVersionTester\root\codeigniter-version-detect\CodeIgniter_1.0b\CodeIgniter_1.0b\system\libraries\Front_controller.php 40
